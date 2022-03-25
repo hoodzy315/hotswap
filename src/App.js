59709242356cb09b80fc20edad1f633432e27ec8
hoodzy315 <jjwoods2@mail.fhsu.edu>
2022-03-25 13:27:20 -0700

    fixed depreciations in packages, started adding image upload handling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import About from "./components/About";
 import ContactUs from "./components/ContactUs";
 import LoggedIn from "./components/LoggedIn";
 import LandingPage from "./components/LandingPage";
+import AddItem from "./components/AddItem";
+
 import { Routes, Route } from "react-router-dom";
+
 
 export const AuthContext = React.createContext();
 const initialState = {
@@ -53,6 +56,7 @@ const App = () => {
                 <Route path='/' element={!state.isAuthenticated ? <LandingPage/> : <LoggedIn/>}/>
                 <Route path='/about' element={<><Navbar /><About /></>} />
                 <Route path='/contactus' element={<><Navbar /><ContactUs /></>} />
+                <Route path='/additem' element={!state.isAuthenticated ? <LandingPage/> : <AddItem/>}/>
             </Routes>
         </div>
         </AuthContext.Provider>
